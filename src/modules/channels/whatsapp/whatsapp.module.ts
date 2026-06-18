c6409f37be327cf669ba35dc236105065834753d
Artag/Chris <scristxyz@gmail.com>
2026-06-17 23:30:44 -0500

    act

--- src/modules/channels/whatsapp/whatsapp.module.ts
+++ src/modules/channels/whatsapp/whatsapp.module.ts
@@ -1,18 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, type Provider } from '@nestjs/common';
 import { CHANNEL_PROVIDERS_TOKEN } from '@core/tokens';
 import { WhatsAppController } from './whatsapp.controller';
 import { WhatsAppAdapter } from './whatsapp.adapter';
 
+const channelProvider: Provider = {
+  provide: CHANNEL_PROVIDERS_TOKEN,
+  useExisting: WhatsAppAdapter,
+  multi: true,
+};
+
 @Module({
   controllers: [WhatsAppController],
-  providers: [
-    WhatsAppAdapter,
-    {
-      provide: CHANNEL_PROVIDERS_TOKEN,
-      useExisting: WhatsAppAdapter,
-      multi: true,
-    },
-  ],
+  providers: [WhatsAppAdapter, channelProvider],
   exports: [WhatsAppAdapter, CHANNEL_PROVIDERS_TOKEN],
 })
 export class WhatsAppModule {}
