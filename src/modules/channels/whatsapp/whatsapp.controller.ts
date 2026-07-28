e0cccea2c7f8a5b8616671c81ff2be434a78bb06
Artag/Chris <scristxyz@gmail.com>
2026-07-28 11:05:08 -0500

    fix whatsapp

--- src/modules/channels/whatsapp/whatsapp.controller.ts
+++ src/modules/channels/whatsapp/whatsapp.controller.ts
@@ -85,7 +85,7 @@ export class WhatsAppController {
       return { status: 'ok' };
     }
 
-    const jobId = `${firstText.externalAccountId}:${firstText.from}`;
+    const jobId = `${firstText.externalAccountId}-${firstText.from}`;
     await this.inboundQueue.add(
       'process',
       {
